a2f2ec477ff1655d753d904d773cf2ccd5ea2462
Ravin06 <77960307+Ravin06@users.noreply.github.com>
2022-01-28 22:15:21 +0800

    Create Senior_or_not.C

---
+++ Senior_or_not.C
@@ -0,0 +1,15 @@
+#include <stdio.h>  
+int main()  
+{  
+    int age;   
+    printf("Enter your age?");   
+    scanf("%d",&age);  
+    if(age>=60)  
+    {  
+        printf("You are a senior...");   
+    }  
+    else   
+    {  
+        printf("You are not a senior...");   
+    }  
+}  
